1b01d0ad5163130bf201e9f50db5a17e17bef354
Damian Kędra <dkedra@esus-it.com>
2024-10-30 07:58:20 +0100

    create buttons component

---
+++ resources/views/components/button/primary.blade.php
@@ -0,0 +1,11 @@
+@props(['disabled' => false])
+
+@php
+    $classes = $disabled ?
+    'cursor-not-allowed opacity-50 text-white bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 focus:outline-none dark:focus:ring-blue-800' :
+    'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
+@endphp
+
+<button @disabled($disabled) {{ $attributes->merge(['class' => $classes]) }}>
+    {{ $slot }}
+</button>
