cc43766a6fe83ed24cbc7202c8c8d4b4c16141be
Damian Kędra <dkedra@esus-it.com>
2024-11-18 18:01:50 +0100

    Create parameter controller template

---
+++ app/Http/Controllers/ParameterController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ParameterController extends Controller
+{
+    public function index()
+    {}
+
+    public function create()
+    {}
+
+    public function edit()
+    {}
+
+    public function store()
+    {}
+
+    public function update()
+    {}
+
+    public function delete()
+    {}
+}
