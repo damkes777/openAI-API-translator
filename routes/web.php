44a512e5e1307aebe444766351ca8603baaac591
Damian Kędra <dkedra@esus-it.com>
2024-11-18 18:02:01 +0100

    Create routes to parameter controller

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ParameterController;
 
 Route::get('/', function () {
     return view('dashboard');
-})->name('dashboard');
+})
+     ->name('dashboard');
 
 Route::controller(ProductController::class)
      ->group(function () {
@@ -23,5 +25,22 @@ Route::controller(ProductController::class)
               ->name('products.store');
          Route::post('products/update', 'update')
               ->name('products.update');
+     });
+
+Route::controller(ParameterController::class)
+     ->prefix('parameters')
+     ->group(function () {
+         Route::get('/', 'index')
+              ->name('parameters.list');
+         Route::get('/create', 'create')
+              ->name('parameters.create');
+         Route::get('/edit/{id}', 'edit')
+              ->name('parameters.edit');
+         Route::put('/store', 'store')
+              ->name('parameters.store');
+         Route::put('/update', 'update')
+              ->name('parameters.update');
+         Route::delete('/delete/{id}')
+              ->name('parameters.delete');
      });
 
